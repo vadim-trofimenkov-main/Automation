8654f678a454ee2ca36f082504d1e110a981c24b
vadim <vadimtrofimenkov97@gmail.com>
2021-02-24 21:53:04 +0300

    Collection update

--- src/main/collection/Task15.java
+++ src/main/collection/Task15.java
@@ -6,32 +6,40 @@ package main.collection;
 // в) Удалить из списка все вхождения заданного к удалению числа;
 // г) Вывести конечный список на консоль.
 
-import java.util.ArrayList;
-import java.util.Scanner;
-
-public class Task15 {
-    public static void main(String[] args) {
-        ArrayList<Integer> arrayList = new ArrayList<>();
-        fulfillList(arrayList);
-        System.out.println("Наш список: " + arrayList);
-        int value = getValueToRemove();
-        System.out.println("Число для удаления: " + value);
-        while (arrayList.contains(value)) {
-            arrayList.remove(Integer.valueOf(value));
-        }
-        System.out.println("Конечный список: " + arrayList);
-    }
-
-    public static void fulfillList(ArrayList<Integer> arrayList) {
-        Scanner scanner = new Scanner(System.in);
-        for (int i = 0; i < 7; i++) {
-            int j = scanner.nextInt();
-            arrayList.add(j);
-        }
-    }
-
-    public static int getValueToRemove() {
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextInt();
-    }
-}
+//import java.util.ArrayList;
+//import java.util.Scanner;
+//
+//public class Task15 {
+//    public static void main(String[] args) {
+//        ArrayList<Integer> arrayList = new ArrayList<>();
+//        fulfillList(arrayList);
+//        printList(arrayList);
+//        int value = getValueToRemove();
+//        removeValue(value, arrayList);
+//        printList(arrayList);
+//    }
+//
+//    public static void fulfillList(ArrayList<Integer> arrayList) {
+//        Scanner scanner = new Scanner(System.in);
+//        for (int i = 0; i < 7; i++) {
+//            int j = scanner.nextInt();
+//            arrayList.add(j);
+//        }
+//    }
+//
+//    public static int getValueToRemove() {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Число для удаления: " + value);
+//        return scanner.nextInt();
+//    }
+//
+//    public static void removeValue(int value, ArrayList<Integer> arrayList) {
+//        while (arrayList.contains(value)) {
+//            arrayList.remove(Integer.valueOf(value));
+//        }
+//    }
+//
+//    public static void printList(ArrayList<Integer> arrayList) {
+//        System.out.println("Наш список: " + arrayList);
+//    }
+//}
